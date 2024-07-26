50f37ab1ad3af2d62cd5da056f238cfb16dd7008
Fredrick Appiah <fredrick.antwi@hubtel.com>
2024-07-26 10:03:33 +0000

    code refactoring

--- chapter_5_todo_app_typescript/src/services/CommonConstants.ts
+++ chapter_5_todo_app_typescript/src/services/CommonConstants.ts
@@ -6,24 +6,13 @@ export type addTaskLType = { taskTitle: string, taskDate: string, completed: boo
 export const localStorageName = "Tasks List";
 export const noTaskMessage = `<p class="no-tasks">You Have 0 Tasks</p>`;
 
-export class SuccessMessage {
-    readonly taskCreatedMessage: string;
-    readonly taskEditedMessage: string;
-    readonly taskDeletedMessage: string;
+export const SuccessMessage = {
+    taskCreatedMessage: "Task Successfully Created",
+    taskEditedMessage: "Task Successfully Edited",
+    taskDeletedMessage: "Task Successfully Deleted"
+} as const;
 
-    constructor() {
-        this.taskEditedMessage = "Task Successfully Created"
-        this.taskCreatedMessage = "Task Successfully Edited";
-        this.taskDeletedMessage = "Task Successfully Deleted";
-    }
-}
-
-export class ActionType {
-    readonly create: [string, string];
-    readonly edit: [string, string];
-
-    constructor() {
-        this.create = ["create", "Create"];
-        this.edit = ["edit", "Edit"];
-    };
+export const ActionType = {
+    create: ["create", "Create"],
+    edit: ["edit", "Edit"]
 }
