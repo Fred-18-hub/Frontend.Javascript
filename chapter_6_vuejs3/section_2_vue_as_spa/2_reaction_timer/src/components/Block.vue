1c70a5a5a73b94b118b484d2b928a87953fa09e6
Fredrick Appiah <fredrick.antwi@hubtel.com>
2024-07-27 17:37:42 +0000

    vuejs reaction timer completed

---
+++ chapter_6_vuejs3/section_2_vue_as_spa/2_reaction_timer/src/components/Block.vue
@@ -0,0 +1,48 @@
+<template>
+    <div v-if="showBlock" class="block" @click="stopTimer">
+        click me
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: [ 'delay' ],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+        }, this.delay)
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            this.$emit('end', this.reactionTime)
+        }
+    }
+}
+</script>
+
+<style>
+    .block {
+        background: #0faf87;
+        width: 400px;
+        color: white;
+        text-align: center;
+        margin: 40px auto;
+        padding: 100px 0;
+        border-radius: 20px;
+    }
+</style>
